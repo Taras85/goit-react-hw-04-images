2471f6c753157c177a17f9b5c0766644f9fc08a6
Taras <topanasenko85@gmail.com>
2023-05-19 18:12:14 +0300

    App full

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -10,99 +10,135 @@ import Button from './Button/Button';
 import Modal from './Modal/Modal';
 
 
-class App extends Component{
-  state = {
-    query: '',
-    images: [],
-    totalImages: 0,
-    largeImageURL: '',
-    page: 1,
-    error: null,
-    isLoading: false,
-    showModal: false,
-  };
+// class App extends Component{
+//   state = {
+//     query: '',
+//     images: [],
+//     totalImages: 0,
+//     largeImageURL: '',
+//     page: 1,
+//     error: null,
+//     isLoading: false,
+//     showModal: false,
+//   };
 
-  async componentDidUpdate(prevProps, prevState) {
+const App =()=>{
+  const [query, setQuery]=useState('')
+  const [images, setImages]=useState([])
+  const [totalImages, setTotalImages]=useState(0)
+  const [largeImageURL, setLargeImageURL]=useState('')
+  const [page, setPage]=useState(1)
+  const [error, setError]=useState(null)
+  const [isLoading, setIsLoading]=useState(false)
+  const [showModal, setShovModal]=useState(false)
+
+  // async componentDidUpdate(prevProps, prevState) {
     
-    if (prevState.query !== this.state.query || prevState.page !== this.state.page) {
-      this.toggleLoader();
+  //   if (prevState.query !== this.state.query || prevState.page !== this.state.page) {
+  //     this.toggleLoader();
 
-      try {
-        const data = await fetchImages(this.state.query, this.state.page);
-        this.setState({ totalImages: data.totalHits });
-        if (data.totalHits === 0) {
-          this.setState({ images: [] });
-          toast.info(`No found  ${this.state.query}. `);
-          return;
-        }
-        this.setState({ images: [...this.state.images, ...data.hits] });
-      } catch (error) {
-        this.setState({ error });
-        toast.error('Oops! Something went wrong');
-      } finally {
-        this.toggleLoader();
-      }
-    }
-  }
+  //     try {
+  //       const data = await fetchImages(this.state.query, this.state.page);
+  //       this.setState({ totalImages: data.totalHits });
+  //       if (data.totalHits === 0) {
+  //         this.setState({ images: [] });
+  //         toast.info(`No found  ${this.state.query}. `);
+  //         return;
+  //       }
+  //       this.setState({ images: [...this.state.images, ...data.hits] });
+  //     } catch (error) {
+  //       this.setState({ error });
+  //       toast.error('Oops! Something went wrong');
+  //     } finally {
+  //       this.toggleLoader();
+  //     }
+  //   }
+  // }
+    useEffect(()=>{
+      if (query){
+            const fetchImg = async ()=>{
+               try {
+                setIsLoading(true)
+                  const data = await fetchImages(query, page);
+                  setTotalImages(data.totalHits)
+                  
+                  if (data.totalHits === 0) {
+                    setImages([])
+                    toast.info(`No found  ${this.state.query}. `);
+                    return;
+                  }
+
+                  setImages(prevImages=> [...prevImages, ...data.hits])
+                 
+                } catch (error) {
+                  setError(error)
+                  // this.setState({ error });
+                  toast.error('Oops! Something went wrong');
+                } finally {
+                  setIsLoading(false)
+                }
+              }
+              fetchImg()
+            }
+    }, [query, page])
 
 
-
-  handleSearchbarSubmit = query => {
-     query !== this.state.query 
-    ? this.setState({ query, page: 1, images: []})
-    : toast.info(`Wealready found images with ${query}`)
+  const handleSearchbarSubmit = query => {
+    setQuery(query)
+    setImages([])
+    setPage(1);
     
   } 
 
-  onLoadMore = () => {
-    if (this.state.images.length === this.state.totalImages){
-      toast.info(`At your request no more photos`)
-    }
-    this.setState(({ page }) => ({
-      page: page + 1,
-    }))
+  const onLoadMore = () => {
+ setPage(prevPage =>prevPage+1)
   }
 
 
 
-  onOpenModal = e => {
-    this.setState({ largeImageURL: e.target.dataset.source });
-    this.toggleModal();
+  const onOpenModal = e => {
+    setLargeImageURL(e.target.dataset.source)
+    // this.setState({ largeImageURL: e.target.dataset.source });
+    setShovModal(true)
   };
+  const closeModal =( )=>{
+    setShovModal(false)
+    setLargeImageURL('')
+  }
 
-  toggleLoader = () => {
-    this.setState(({ isLoading }) => ({
-      isLoading: !isLoading,
-    }));
-  };
+  // const toggleLoader = () => {
+  //   this.setState(({ isLoading }) => ({
+  //     isLoading: !isLoading,
+  //   }));
+  // };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-  };
+  // const toggleModal = () => {
+  //   this.setState(({ showModal }) => ({
+  //     showModal: !showModal,
+  //   }));
+  // };
 
 
-  render() {
-    const { images, largeImageURL, isLoading, showModal, error } = this.state;
+  // render() {
+  //   const { images, largeImageURL, isLoading, showModal, error } = this.state;
     return (
       <div>
         <Searchbar
-          onSubmit={this.handleSearchbarSubmit}
+          onSubmit={handleSearchbarSubmit}
         />
         {error}
 
          {images.length > 0 && !error && (
-          <ImageGallary images={images} error={error} onOpenModal={this.onOpenModal} />
+          <ImageGallary images={images} error={error} onOpenModal={onOpenModal} />
         )}
 
         {isLoading && <Loader/>}
         
         {!isLoading && images.length >= 12 && !error &&
-          (<Button onLoadMore={this.onLoadMore} />)}
+          (<Button onLoadMore={onLoadMore} />)}
         
         {showModal && (
-          <Modal onToggleModal={this.toggleModal}
+          <Modal onToggleModal={closeModal}
             largeImageURL={largeImageURL} />
         )}
 
@@ -110,6 +146,6 @@ class App extends Component{
       </div>
     )
   }
-}
+// }
 
 export default App;
